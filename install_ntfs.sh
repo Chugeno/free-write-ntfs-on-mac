a30831c4f5b8c3cc62d4c3e582a39cb848b1b2d3
Eugenio <eugenioazurmendi@gmail.com>
2025-06-10 14:54:28 -0300

    Actualizo script auto_mount_ntfs.workflow y deteccion version macports

--- install_ntfs.sh
+++ install_ntfs.sh
@@ -32,19 +32,39 @@ OS_VERSION=$(sw_vers -productVersion)
 MAJOR_VERSION=$(echo $OS_VERSION | cut -d '.' -f 1)
 MINOR_VERSION=$(echo $OS_VERSION | cut -d '.' -f 2)
 
-# Determinar el enlace de MacPorts según la versión de macOS
-if [[ "$MAJOR_VERSION" -eq 15 ]]; then
-    MACPORTS_URL="https://github.com/macports/macports-base/releases/download/v2.10.5/MacPorts-2.10.5-15-Sequoia.pkg"
-elif [[ "$MAJOR_VERSION" -eq 14 ]]; then
-    MACPORTS_URL="https://github.com/macports/macports-base/releases/download/v2.10.5/MacPorts-2.10.5-14-Sonoma.pkg"
-elif [[ "$MAJOR_VERSION" -eq 13 ]]; then
-    MACPORTS_URL="https://github.com/macports/macports-base/releases/download/v2.10.5/MacPorts-2.10.5-13-Ventura.pkg"
-elif [[ "$MAJOR_VERSION" -eq 12 ]]; then
-    MACPORTS_URL="https://github.com/macports/macports-base/releases/download/v2.10.5/MacPorts-2.10.5-12-Monterey.pkg"
-else
-    echo -e "${RED}Versión de macOS no soportada. Salida.${NC}"
-    exit 1
-fi
+# Determine MacPorts URL based on macOS version
+# if [[ "$MAJOR_VERSION" -eq 15 ]]; then
+#     MACPORTS_URL="https://github.com/macports/macports-base/releases/download/v2.10.5/MacPorts-2.10.5-15-Sequoia.pkg"
+# elif [[ "$MAJOR_VERSION" -eq 14 ]]; then
+#     MACPORTS_URL="https://github.com/macports/macports-base/releases/download/v2.10.5/MacPorts-2.10.5-14-Sonoma.pkg"
+# elif [[ "$MAJOR_VERSION" -eq 13 ]]; then
+#     MACPORTS_URL="https://github.com/macports/macports-base/releases/download/v2.10.5/MacPorts-2.10.5-13-Ventura.pkg"
+# elif [[ "$MAJOR_VERSION" -eq 12 ]]; then
+#     MACPORTS_URL="https://github.com/macports/macports-base/releases/download/v2.10.5/MacPorts-2.10.5-12-Monterey.pkg"
+# else
+#     echo -e "${RED}Versión de macOS no soportada. Salida.${NC}"
+#     exit 1
+# fi
+
+MACOS_VERSION=$(sw_vers -productVersion | cut -d '.' -f 1)
+MAJOR_VERSION=$MACOS_VERSION
+
+# Detectar el nombre de macOS (esto lo necesitás para construir el nombre del archivo)
+case "$MAJOR_VERSION" in
+  15) MACOS_NAME="15-Sequoia" ;;
+  14) MACOS_NAME="14-Sonoma" ;;
+  13) MACOS_NAME="13-Ventura" ;;
+  12) MACOS_NAME="12-Monterey" ;;
+  *) echo "macOS no soportado"; exit 1 ;;
+esac
+
+# Obtener la última versión de MacPorts (por tag)
+LATEST_TAG=$(curl -s https://api.github.com/repos/macports/macports-base/releases/latest | grep '"tag_name":' | cut -d '"' -f4)
+
+# Construir la URL de descarga
+MACPORTS_URL="https://github.com/macports/macports-base/releases/download/$LATEST_TAG/MacPorts-${LATEST_TAG#v}-${MACOS_NAME}.pkg"
+
+echo "Descargando desde: $MACPORTS_URL"
 
 # Instalar MacPorts
 echo -e "${GREEN}Instalando MacPorts desde $MACPORTS_URL...${NC}"
@@ -61,6 +81,6 @@ echo "y" | sudo port install ntfs-3g
 
 # Copiar el archivo de Automator
 echo -e "${GREEN}Copiando el archivo de Automator...${NC}"
-cp ./auto_mount_ntfs.workflow ~/Library/Workflows/Applications/Folder\ Actions/
+cp -R ./auto_mount_ntfs.workflow ~/Library/Workflows/Applications/Folder\ Actions/
 
 echo -e "${GREEN}Instalación completada. Por favor, verifica que todo esté funcionando correctamente.${NC}"
